ac65964936155ad208598ce1b74fc13db69a2300
Gupta <shubhg3@illinois.edu>
2022-10-14 14:23:52 -0500

    adding IDT files

---
+++ student-distrib/idt.c
@@ -0,0 +1,179 @@
+#include "idt.h"
+// #include ""
+
+#define LAST_EXCEPTION 0xF  // defined as the last software generated exception which is 15  
+
+// typedef void (*functions);
+
+
+// function[15] = ["divide_by_zero",
+//                   "single_step_int",
+//                   "non_maskable_int",
+//                   "breakpoint",
+//                   "overflow"
+//                   "bound_range_exceeds"
+//                   "invalid_opcode"
+//                   "coprocessor_not_avail"
+//                   "double_fault"
+//                   ""
+//                   ""
+//                   ""
+// ]
+
+// read IA-32 Intel page 153, 5.10 -- helps with present bit 
+
+// 31_____________16_15_14_13_________8_7_____5_4_________0
+// |               |   | D |           |       |          |
+// |  offset 31_16 | P | P | 0 D 1 1 0 | 0 0 0 |          |
+// |               |   | L |           |       |          |
+// --------------------------------------------------------
+
+void idt_initialization(){
+    int i;
+    for(i = 0; i<256; i++){
+        if (i <= LAST_EXCEPTION && i != 0xF){
+            idt[i].dpl = 0;
+            idt[i].present = 1;
+        }
+        else{
+            idt[i].present = 0;
+        }
+        idt[i].reserved0 = 0;   
+        idt[i].size = 1 ;       // D bit is set to 1, look above
+        idt[i].reserved1 = 1 ;
+        idt[i].reserved2 = 1 ;
+        idt[i].reserved3 = 0 ;
+        idt[i].reserved4 &= 0x1F;
+        if(i == 0x80){
+            idt[i].dpl = 3;
+            idt[i].present = 1;
+            // idt[i].reserved3 = 1;
+        }
+        idt[i].seg_selector = KERNEL_CS; // setting the segment selector to the kernel code segment
+    }
+    SET_IDT_ENTRY(idt[0], divide_by_zero);
+    SET_IDT_ENTRY(idt[1], single_step_int);
+    SET_IDT_ENTRY(idt[2], non_maskable_int);  
+    SET_IDT_ENTRY(idt[3], breakpoint);
+    SET_IDT_ENTRY(idt[4], overflow);
+    SET_IDT_ENTRY(idt[5], bound_range_exceeds);
+    SET_IDT_ENTRY(idt[6], invalid_opcode);
+    SET_IDT_ENTRY(idt[7], coprocessor_not_avail);
+    SET_IDT_ENTRY(idt[8], double_fault);
+    SET_IDT_ENTRY(idt[9], coprocessor_segment_overrun);
+    SET_IDT_ENTRY(idt[10], invalid_task_state);
+    SET_IDT_ENTRY(idt[11], segment_not_present);
+    SET_IDT_ENTRY(idt[12], stack_segment_fault);
+    SET_IDT_ENTRY(idt[13], general_protection_fault);
+    SET_IDT_ENTRY(idt[14], page_fault);
+    // SET_IDT_ENTRY(idt[15], handler);
+    SET_IDT_ENTRY(idt[16], floating_point_exception);
+    lidt(idt_desc_ptr);
+    return;
+}
+extern void divide_by_zero(){
+    printf("Exception: Division by Zero\n");
+    while(1);
+   
+}
+// 0x1
+extern void single_step_int(){
+    printf("Exception: Division by Zero\n");
+    while(1);
+   
+}     
+// 0x2
+extern void non_maskable_int(){
+    printf("Exception: Division by Zero\n");
+    while(1);
+   
+}      
+extern void breakpoint(){
+    printf("Exception: Division by Zero\n");
+    while(1);
+   
+}     
+extern void overflow(){
+    printf("Exception: Division by Zero\n");
+    while(1);
+   
+}  
+extern void bound_range_exceeds(){
+    printf("Exception: Division by Zero\n");
+    while(1);
+   
+}      
+extern void invalid_opcode(){
+    printf("Exception: Division by Zero\n");
+    while(1);
+   
+}     
+extern void coprocessor_not_avail(){
+    printf("Exception: Division by Zero\n");
+    while(1);
+   
+}   
+extern void double_fault(){
+    printf("Exception: Division by Zero\n");
+    while(1);
+   
+}      
+extern void coprocessor_segment_overrun(){
+    printf("Exception: Division by Zero\n");
+    while(1);
+   
+}     
+extern void invalid_task_state(){
+    printf("Exception: Division by Zero\n");
+    while(1);
+   
+}  
+extern void segment_not_present(){
+    printf("Exception: Division by Zero\n");
+    while(1);
+   
+}      
+extern void stack_segment_fault(){
+    printf("Exception: Division by Zero\n");
+    while(1);
+   
+}  
+extern void general_protection_fault(){
+    printf("Exception: Division by Zero\n");
+    while(1);
+   
+}
+extern void page_fault(){
+    printf("Exception: Division by Zero\n");
+    while(1);
+   
+}
+// 0xF reserved 
+
+//0x10
+extern void floating_point_exception(){
+    printf("Exception: Division by Zero\n");
+    while(1);
+   
+}
+// int main(){
+// SET_IDT_ENTRY(idt[0], divide_by_zero);
+// SET_IDT_ENTRY(idt[1], single_step_int);
+// SET_IDT_ENTRY(idt[2], non_maskable_int);  
+// SET_IDT_ENTRY(idt[3], breakpoint);
+// SET_IDT_ENTRY(idt[4], overflow);
+// SET_IDT_ENTRY(idt[5], bound_range_exceeds);
+// SET_IDT_ENTRY(idt[6], invalid_opcode);
+// SET_IDT_ENTRY(idt[7], coprocessor_not_avail);
+// SET_IDT_ENTRY(idt[8], double_fault);
+// SET_IDT_ENTRY(idt[9], coprocessor_segment_overrun);
+// SET_IDT_ENTRY(idt[10], invalid_task_state);
+// SET_IDT_ENTRY(idt[11], segment_not_present);
+// SET_IDT_ENTRY(idt[12], stack_segment_fault);
+// SET_IDT_ENTRY(idt[13], general_protection_fault);
+// SET_IDT_ENTRY(idt[14], page_fault);
+// // SET_IDT_ENTRY(idt[15], handler);
+// SET_IDT_ENTRY(idt[16], floating_point_exception);
+// lidt(idt_desc_ptr);
+// return 0;
+// }
---
+++ student-distrib/idt.h
@@ -0,0 +1,32 @@
+#include "types.h"
+#include "x86_desc.h"
+#include "multiboot.h"
+#include "lib.h"
+
+
+
+
+extern void idt_initialization();
+
+extern void divide_by_zero();  // 0x0
+extern void single_step_int(); // 0x1      
+extern void non_maskable_int(); // 0x2      
+extern void breakpoint(); // 0x3      
+extern void overflow(); // 0x4      
+extern void bound_range_exceeds(); // 0x5      
+extern void invalid_opcode(); // 0x6      
+extern void coprocessor_not_avail(); // 0x7      
+extern void double_fault(); // 0x8      
+extern void coprocessor_segment_overrun(); // 0x9      
+extern void invalid_task_state(); // 0xA      
+extern void segment_not_present(); // 0xB      
+extern void stack_segment_fault(); // 0xC   
+extern void general_protection_fault(); // 0xD
+extern void page_fault(); // 0xE
+// 0xF reserved 
+extern void floating_point_exception(); // 0x10 
+
+
+
+
+
