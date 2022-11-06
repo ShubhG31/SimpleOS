a17dfd244d8d8f814dd30ecfbf050c2f4e2be56f
aditiaa2 <aditiaa2@illinois.edu>
2022-11-06 17:46:22 -0600

    added comments

--- student-distrib/system_call.h
+++ student-distrib/system_call.h
@@ -9,12 +9,15 @@
 #define HALT 37
 #define HALT_error 256
 
+//describes and takes in what is necessary for the file descriptor
 struct file_descriptor{
     uint32_t opt_table_pointer;
     uint32_t inode;
     uint32_t file_pos;
     uint32_t flags;
 };
+
+//describes what is necessary for the pcb table
 struct PCB_table{
     int8_t id;                 // 1 byte
     int8_t parent_id;            // 1 byte
@@ -24,21 +27,50 @@ struct PCB_table{
     int8_t fdt_usage; //00000011// 1 byte
     struct file_descriptor fdt[8]; // 16 byte each
 };
+
+//the function that represents the system call handler
 extern int system_call_handler();
 
+//function that initializes the file descriptor
 extern void fd_init();
+
+//function that halts the system
 extern int system_halt(uint8_t status);
-extern int system_execute(const uint8_t* command); 
+
+//function that executes the system
+extern int system_execute(const uint8_t* command);
+
+//function for the reading of the system
 extern int system_read(int32_t fd, void* buf, int32_t nbytes); 
+
+//function for the writing of the system
 extern int system_write(int32_t fd, const void* buf, int32_t nbytes); 
+
+//function for the opening of the system
 extern int system_open(const uint8_t* filename); 
+
+//function for the closing of the system
 extern int system_close(int32_t fd);
+
+//function that gets the arguments for the system
 extern int system_getargs(uint8_t* buf, int32_t nbytes); 
+
+//function separate for video memory
 extern int system_vidmap(uint8_t** screen_start);
+
+//function that gets the pcb pointer
 extern int get_pcb_pointer();
+
+//function that checks if the file descriptor is in use
 int check_fd_in_use(int32_t fd);
+
+//function that gets the pid
 extern int get_pid();
+
+//function that prepares the IRET
 extern void IRET_prepare(int);
+
+//function for an extra label
 extern void label();
 
 #endif
