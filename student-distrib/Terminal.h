3d47ba823a3236bcda73d2c458732205c60a72e1
aditiaa2 <aditiaa2@illinois.edu>
2022-11-06 17:04:16 -0600

    added comments

--- student-distrib/Terminal.h
+++ student-distrib/Terminal.h
@@ -4,6 +4,8 @@
 #include "lib.h"
 #include "types.h" 
 
+//functions written used for terminal.c read, write, open and close
+
 uint32_t terminal_read(int32_t fd, void *buf, int32_t nbytes);
 
 uint32_t terminal_write(int32_t fd, const void* buf, int32_t nbytes);
@@ -13,6 +15,5 @@ uint32_t terminal_open(const int8_t* filename);
 uint32_t terminal_close(int32_t fd);
 
 void copy_buffer(char* keyboard);
-
 
 #endif
