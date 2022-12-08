d06eef6abf132b42603a514360830ade85c104fe
snarra2 <snarra2@illinois.edu>
2022-12-08 12:24:45 -0600

    up and down arrow problem with large strings

--- student-distrib/keyboard.h
+++ student-distrib/keyboard.h
@@ -11,6 +11,7 @@
 #define keyboard_irq_num 0x01
 
 extern char buffer[3][128];
+// extern char past_entries[3][5][128];
 // volatile static int newline_flag = 0; 
 
 void keyboard_init_irq(); // intialize the keyboard
@@ -19,5 +20,9 @@ extern void keyboard_handler();
 void keyboard_helper();
 
 extern int get_enter_flag(int terminal_);
+
+extern int prev_arrow_pressed();
+
+extern int len_prev();
 
 #endif // _KEYBOARD_
